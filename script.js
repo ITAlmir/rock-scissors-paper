d97b7483e3f6978ced3f12fd81dd1ab485751b96
Almir <almiredin.redzic@gmail.com>
2023-08-28 15:15:33 +0200

    new commit

--- script.js
+++ script.js
@@ -19,17 +19,34 @@ function showSlides() {
   setTimeout(showSlides, 200);
 }
 
-const thumbnails = document.querySelectorAll(".thumbnail");
-const largeImage = document.querySelector(".large-image");
-const largeCpuImage = document.getElementById("comp-lage-img");
+let thumbnails = document.querySelectorAll(".thumbnail");
+let largeImage = document.querySelector(".large-image");
+let largeCpuImage = document.getElementById("comp-lage-img");
 let slideShow = document.getElementById("slideshow-container");
 let compContainer = document.getElementById("comp-container");
+let roundsForm = document.getElementById("round-count");
+let input = document.getElementById('rounds');
+let submitButton = document.getElementById('submit-btn');
+let imgElement = document.getElementById('transition-img');
+let gameSection = document.getElementById('game-section');
+let compScoreParagraph = document.getElementsByClassName("cpu-score");
+let myScoreParagraph = document.getElementById("my-score");
+let roundsLeftParagraph = document.getElementById("rounds-left");
+let gameTipsParagraph = document.getElementById("game-tips");
+let upperGameTipsParagraph = document.getElementById("game-tips-upper");
+let divElement = document.getElementById('game-icons');
+
 let isClickable = true;
 let myScore = 0;
 let compScore = 0;
+let roundCounter = 0;
 
 thumbnails.forEach(thumbnail => {
   thumbnail.addEventListener("click", () => {
+    if(roundCounter > 0){
+      upperGameTipsParagraph.classList.add("hidden");
+      slideShow.classList.remove("hidden");
+    compContainer.classList.add("hidden");
     if (isClickable) {
         isClickable = false; 
     largeImage.src = thumbnail.src;
@@ -41,10 +58,49 @@ thumbnails.forEach(thumbnail => {
     slideShow.classList.add("hidden");
     compContainer.classList.remove("hidden");
     isClickable = true;
-
     showScores(thumbnail.id,randomIndex);
+    upperGameTipsParagraph.classList.remove("hidden");
 }, 3000);
-    }    
+    }
+    }else{
+      hihglightText = "Game Over !";
+            noProgress(hihglightText);
+    }
+    
+  });
+});
+
+function startNewGame(){
+  roundsForm.classList.remove("hidden");
+}
+
+document.addEventListener('DOMContentLoaded', function() {  
+
+  myScore = 0;
+  compScore = 0;
+  
+  let submitButton = document.getElementById('submit-btn');
+  submitButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    let userInputValue = input.value;
+    if(userInputValue ===""){
+      hihglightText = "Please enter number of round !";
+            noProgress(hihglightText);
+    }else if(isNaN(userInputValue)){
+      hihglightText = "Please use numbers !";
+            noProgress(hihglightText);
+    } else{
+    setInterval(changeBackgroundColor, 50);
+    upperGameTipsParagraph.classList.remove("hidden");
+    roundCounter = userInputValue;
+    roundsLeftParagraph.innerHTML="Rounds Left:"+roundCounter;
+    roundsForm.classList.add("hidden");
+    imgElement.style.width = '10%';
+    gameSection.classList.remove('hidden');
+    gameTipsParagraph.classList.add("hidden");
+
+  }
   });
 });
 
@@ -58,38 +114,87 @@ function showScores(num1,num2){
         case "0":
           // Handle case for image 1
           if(num2 == 0){
+            hihglightText = "It's a tie";
              myScore = myScore;
              compScore = compScore;
-          }if (num2 == 1) {
+          }else if(num2 == 1) {
+            hihglightText = "The CPU scored";
             compScore++; 
           } else {
+            hihglightText = "You scored";
             myScore++;
           }
           break;
         case "1":
             if(num2 == 0){
+              hihglightText = "You scored";
                 myScore++;
-             }if (num2 == 1) {
+             }else if (num2 == 1) {
+              hihglightText = "It's a tie";
                 myScore = myScore;
                 compScore = compScore;
              } else {
+                hihglightText = "The CPU scored";
                 compScore++; 
              }
           break;
         case "2":
             if(num2 == 0){
-                compScore++; 
-             }if (num2 == 1) {
-                myScore++;
+              hihglightText = "The CPU scored";
+                compScore++;
+             }else if (num2 == 1) {
+              hihglightText = "You scored";
+              myScore++;
              } else {
-                myScore = myScore;
+              hihglightText = "It's a tie";
+              myScore = myScore;
                 compScore = compScore;
              }
           break;
         default:
-          // Default case if id doesn't match any of the above
+          console.log("No match");
           break;
       }
-      console.log(myScore);
-      console.log(compScore);
+      myScoreParagraph.innerHTML=myScore;
+      compScoreParagraph[0].innerHTML=compScore;
+      compScoreParagraph[1].innerHTML=compScore;
+      noProgress(hihglightText);
+      roundCounter--;
+      roundsLeftParagraph.innerHTML="Rounds Left:"+roundCounter;
+      let won = "";
+      if(roundCounter == 0){
+        if(compScore > myScore){
+           won = "CPU Won! ";
+        }else if(myScore >compScore){
+          won = "You won !"
+        }else{
+          won = "It's a tie !";
+        }
+        gameTipsText = "Game Over! <br>"+ won +"<br> Please start new game!";
+        gameTipsParagraph.classList.remove("hidden");
+        gameTips(gameTipsText);
+      }
+
+}
+function noProgress(hihglightText){
+  let highlightedElements = document.getElementById("highlight");
+      highlightedElements.innerHTML = hihglightText;
+      highlightedElements.classList.remove("hidden");
+      setTimeout(() => {
+          highlightedElements.classList.add("hidden");
+      }, 3000);
+}
+function gameTips(gameTipsText){
+    gameTipsParagraph.innerHTML = gameTipsText;
+}
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function changeBackgroundColor() {
+  divElement.style.backgroundColor = getRandomColor();
 }
